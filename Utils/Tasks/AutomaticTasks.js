a2c8d0c85569fb3670f1f1fc621798002cd61633
Music Maker <joshuanejdl@gmail.com>
2025-10-10 23:22:59 -0500

    refactor: Updated AutomaticTasks to be more flexible

--- Utils/Tasks/AutomaticTasks.js
+++ Utils/Tasks/AutomaticTasks.js
@@ -1,68 +1,58 @@
 const { SECONDS } = require("../Constants");
 const Database = require("../Database");
-const { warn, error, success } = require("../Logs");
+const Log = require("../Logs");
 const TaskScheduler = require("../TaskScheduler");
 
 const TIME_BETWEEN_TASKS = SECONDS.MINUTE * 10; // 10 minutes
 
-const TASK = {
+const TASKS = [
+	[ 'temp_role_access',	require("./TemporaryRoleAccess.js"), 5 ], // run every 5 seconds
+];
 
+for (let i = 0; i < TASKS.length; i++) {
+	TASKS[i][2] *= 1000; // convert interval to milliseconds
 }
 
-const TaskFunctions = {
+const longestName = Math.max( ... TASKS.map(t => t[0].length) );
 
-}
-
-const TASK_INTERVAL = {
-
-}
-
-let longestName = 0;
-
-let i = 0;
-for (const [name, callback] of Object.entries(TaskFunctions)) {
-	i++;
-	if (name === undefined) {
-		warn(`Task ${i} is undefined, skipping...`);
-		delete TaskFunctions[name];
-		return;
-	}
-
-	if (name.length > longestName) {
-		longestName = name.length;
-	}
-
-	if (typeof callback !== 'function') {
-		warn(`Task "${name}" is not a function, skipping...`);
-		delete TaskFunctions[name];
-		continue;
-	}
-
-	TASK_INTERVAL[name] *= 1000; // convert to milliseconds
-}
-
-module.exports.StartTasks = function StartTasks() {
-	const totalTasks = Object.keys(TaskFunctions).length;
+module.exports.StartTasks = async function StartTasks() {
+	const totalTasks = TASKS.length;
 	if (totalTasks === 0) {
-		warn("No tasks to manage - nothing to do!");
+		Log.warn("No tasks to manage - nothing to do!");
 		return;
 	} else {
-		success(`Starting ${totalTasks} automatic tasks...`);
+		Log.success(`Starting ${totalTasks} automatic tasks...`);
 	}
 
-	let i = -1;
-	for (const [name, callback] of Object.entries(TaskFunctions)) {
-		i++;
-		const interval = TASK_INTERVAL[name];
-		if (interval === undefined) {
-			warn(`Task "${name}" does not have a defined interval, skipping...`);
+	const selectQuery = Database.prepare("SELECT last_run FROM Timers WHERE id = ?");
+
+	for (let i = 0; i < TASKS.length; i++) {
+		const taskData = TASKS[i];
+		if (!Array.isArray(taskData) || taskData.length !== 3) {
+			Log.warn(`Task entry ${i} is not an array, skipping...`);
+			continue;
+		}
+		const [ name, callback, interval ] = taskData;
+
+		if (typeof interval !== 'number' || interval <= 0 || !Number.isFinite(interval)) {
+			Log.warn(`Task "${name}" does not have a defined interval, skipping...`);
+			continue;
+		}
+		if (typeof callback !== 'function') {
+			Log.warn(`Task "${name}" does not have a callback function, skipping...`);
+			continue;
+		}
+		if (callback.constructor.name !== 'AsyncFunction') {
+			Log.warn(`Task "${name}" callback must be an async function, skipping...`);
 			continue;
 		}
 
-		const lastRun = Database.prepare("SELECT last_run FROM Timers WHERE id = ?").pluck().get(name) || 0;
+		const lastRun = selectQuery.pluck().get(name);
+		// rounding errors shouldn't be a concern because the exact second doesn't matter
+		const lastRunNumber = Number(lastRun);
 
 		const now = Date.now();
-		const timeSinceLastRun = Math.max(0, now - lastRun);
+		const timeSinceLastRun = Math.max(0, now - lastRunNumber);
 
 		const offset = (TIME_BETWEEN_TASKS * 1000) * i;
 		const delay = Math.max(timeSinceLastRun >= interval ? 0 : interval - timeSinceLastRun, offset);
@@ -70,14 +60,12 @@ module.exports.StartTasks = function StartTasks() {
 		TaskScheduler.schedule(() => {
 			try {
 				callback();
-				Database.prepare("INSERT OR REPLACE INTO Timers (id, last_run) VALUES (?, ?)").run(name, Date.now());
+				Database.prepare("INSERT INTO Timers (id, last_run) VALUES (?, ?) ON CONFLICT DO UPDATE SET last_run = excluded.last_run").run(name, Date.now());
 			} catch (err) {
-				error(err);
+				Log.error(err);
 			}
 		}, delay, interval);
 
-		// success(`Scheduled task "${name}" to run every ${interval / 1000} seconds (delayed by ${delay / 1000 / 60} minutes)`);
-
-		success(`[TASKS] - "${name}"${' '.repeat(longestName - name.length)} : delayed ${(delay / 1000 / 60).toFixed(2)} minutes`);
+		Log.success(`[TASKS] - "${name}"${' '.repeat(longestName - name.length + 2)} : delayed ${(delay / 1000 / 60).toFixed(2)} minutes`);
 	}
 }
