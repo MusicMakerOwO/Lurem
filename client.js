bd5456d5c59d8b5e2db39f3fb82a75eb339e85ec
Music Maker <joshuanejdl@gmail.com>
2025-10-15 00:03:52 -0500

    feat: Added additional intents and partials in preparation for modmail

--- client.js
+++ client.js
@@ -4,7 +4,11 @@ module.exports = new Client({
 	intents: [
 		'Guilds',
 		'GuildMembers',
+		'GuildMessages',
 		'MessageContent',
 		'DirectMessages'
-	]
+	],
+	// DJS doesn't take string inputs like intents
+	// This is partials for channels and messages, required for DMs
+	partials: [ 1, 3 ]
 });
