0b2de28ae51a1cfac74743803a9fa427ae193d33
Music Maker <joshuanejdl@gmail.com>
2025-10-15 02:01:58 -0500

    feat: Added a default value to Modmail.created_at in the schema

--- DB_SETUP.sql
+++ DB_SETUP.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS Timers (
 ) STRICT;
 
 CREATE TABLE IF NOT EXISTS Modmail (
-	channel_id TEXT NOT NULL PRIMARY KEY,
+	channel_id TEXT NOT NULL PRIMARY KEY, -- Thread channel ID, parent channel can be found in GuildSettings.modmail_channel_id
 	user_id TEXT NOT NULL UNIQUE,
-	created_at INT NOT NULL
+	created_at INT NOT NULL DEFAULT (UNIXEPOCH('now', 'localtime') * 1000) -- store as ms (JS uses ms timestamps)
 ) STRICT;
 CREATE INDEX IF NOT EXISTS idx_user_id ON Modmail (user_id);
 
