9c03721bdfa6b5c69dfc4cb84a36b806f02e5996
Music Maker <joshuanejdl@gmail.com>
2025-10-29 12:35:46 -0500

    feat: TaskScheduler now returns task IDs and can cancel/restart them

--- Utils/TaskScheduler.js
+++ Utils/TaskScheduler.js
@@ -3,6 +3,8 @@ class TaskScheduler {
         this.taskQueue = [];
         this.timeout = null;
 
+		this.nextTaskID = 1;
+
 		// dummy task, this lets you break past the 24.8 days limit on setTimeout()
 		this.schedule(() => {}, 2147483647);
     }
@@ -13,16 +15,38 @@ class TaskScheduler {
 		if (interval && typeof interval !== 'number') throw new TypeError('Interval must be a number');
 
 		if (interval === undefined) interval = delay;
-		
-        const task = { callback, time: Date.now() + delay, interval };
+
+		if (this.nextTaskID === NUMBER.MAX_SAFE_INTEGER) throw new Error('Too many scheduled tasks, cannot schedule more');
+
+		const taskID = this.nextTaskID++;
+
+        const task = { id: taskID, callback, time: Date.now() + delay, interval };
         this.taskQueue.push(task);
         this.taskQueue.sort((a, b) => a.time - b.time);
         this.#reschedule();
+
+		return taskID;
     }
+
+	cancel(taskID) {
+		this.taskQueue = this.taskQueue.filter(task => task.id !== taskID);
+		this.#reschedule();
+	}
+
+	restartTask(taskID, delay, interval) {
+		const task = this.taskQueue.find(task => task.id === taskID);
+		if (!task) throw new Error('Task not found');
+
+		task.time = Date.now() + delay;
+		if (interval !== undefined) task.interval = interval;
+
+		this.taskQueue.sort((a, b) => a.time - b.time);
+		this.#reschedule();
+	}
 
     #reschedule() {
         if (this.timeout) clearTimeout(this.timeout);
-        
+
         const nextTask = this.taskQueue[0];
         const timeUntilNext = Math.max(0, nextTask.time - Date.now());
 
